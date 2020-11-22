f8b56170a54ca4f398165aa583742581685f97f6
serhiodev90 <lokom286@yandex.ru>
2020-11-23 00:43:47 +0300

    Adding a slider

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$('.carousel__inner').slick({
+		speed: 1200,
+		draggable: false,
+		touchThreshold: 3,
+		prevArrow: '<button type="button" class="slick-prev"><img src="/icons/slider/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="/icons/slider/right.png"></button>',
+		responsive: [
+			{
+				breakpoint: 1200,
+				settings: {
+					dots: true,
+					arrows: false
+				}
+			}
+		]
+	});
+});		
