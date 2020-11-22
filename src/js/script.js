b79647e435ce44ec5f6bf4a13caf3ad1fbccffb8
serhiodev90 <lokom286@yandex.ru>
2020-11-23 01:32:35 +0300

    Edit the paths to the icons of the slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
 		speed: 1200,
 		draggable: false,
 		touchThreshold: 3,
-		prevArrow: '<button type="button" class="slick-prev"><img src="/icons/slider/left.png"></button>',
-		nextArrow: '<button type="button" class="slick-next"><img src="/icons/slider/right.png"></button>',
+		prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/left.png"></button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/right.png"></button>',
 		responsive: [
 			{
 				breakpoint: 1200,
