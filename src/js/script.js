cb4b7b3b72bf44f8702054760b8d2aeafdfef7fc
serhiodev90 <lokom286@yandex.ru>
2020-12-02 00:34:54 +0300

    Setting send mails

--- src/js/script.js
+++ src/js/script.js
@@ -81,4 +81,21 @@ $(document).ready(function () {
 	validateForms('#consultation-form');
 	validateForms('#consultation form');
 	validateForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "../mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #oreder').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
 });		
